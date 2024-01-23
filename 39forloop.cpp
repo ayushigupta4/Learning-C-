becf54866e4acabb67a520eb114ddc6ed62e7e26
Ayushi <ayushi.gupta.038@gmail.com>
2024-01-23 21:04:37 +0530

    Bitwise and for-loop programs

---
+++ 39forloop.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter n: ";
+    cin >> n;
+
+    for(int i = 1; i <= n; i++){
+        cout << i << endl;
+    }
+
+    for(int a=0, b=1; a>=0 && b<=n; a++, b++){
+        cout << a << " " << b << endl;
+    }
+}
